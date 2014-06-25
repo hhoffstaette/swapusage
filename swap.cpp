c3120745180c2fbb2551e80490d6817250c4ac12
Holger Hoffstätte <holger.hoffstaette@googlemail.com>
2014-06-25 21:24:22 +0200

    efficiently skip lines with 0 swap

--- swap.cpp
+++ swap.cpp
@@ -1,4 +1,5 @@
 
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <stdio.h>
@@ -8,21 +9,23 @@
 
 using namespace std;
 
-static const string SWAP_PREFIX = "Swap:";
+static const string PREFIX = "Swap:";
+static const string SUFFIX = "0 kB";
 
-static long get_swap_for_smaps_fragment(const string& line)
+static bool has_swap(const string& line)
 {
-	if (!line.compare(0, SWAP_PREFIX.length(), SWAP_PREFIX))
+	return (line.compare(0, PREFIX.length(), PREFIX) == 0)
+	&& !(line.compare(line.length() - SUFFIX.length(), SUFFIX.length(), SUFFIX) == 0);
+}
+
+static long get_swap(const string& line)
+{
+	const string value = substring_at(line, " ", 1);
+	long kb = 0;
+
+	if (sscanf(value.c_str(), "%ld", &kb) == 1)
 	{
-		const string value = substring_at(line, " ", 1);
-		if (value != "0")
-		{
-			long kb = 0;
-			if (sscanf(value.c_str(), "%ld", &kb) == 1)
-			{
-				return kb;
-			}
-		}
+		return kb;
 	}
 
 	return 0;
@@ -31,16 +34,18 @@ static long get_swap_for_smaps_fragment(const string& line)
 long get_swap_for_pid(const string& pid)
 {
 	const string filename = "/proc/" + pid + "/smaps";
-	long swap = -1;
+	long swap = 0;
 
 	ifstream in(filename);
 	if (in.is_open())
 	{
-		swap = 0;
 		string line;
 		while (getline(in, line))
 		{
-			swap += get_swap_for_smaps_fragment(line);
+			if (has_swap(line))
+			{
+				swap += get_swap(line);
+			}
 		}
 
 		in.close();
