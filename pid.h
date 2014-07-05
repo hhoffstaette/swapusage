5cbd4523c2c30d7ece2a5c3826a23577e357d9d2
Holger Hoffstätte <holger.hoffstaette@googlemail.com>
2014-07-05 15:14:14 +0200

    declare consistent type for unknown pid

--- pid.h
+++ pid.h
@@ -6,7 +6,7 @@
 #include <string>
 
 // marks an illegal pid_t value
-static const int UNKNOWN_PID = std::numeric_limits<pid_t>::max();
+static const pid_t UNKNOWN_PID = std::numeric_limits<pid_t>::max();
 
 // returns value as pid or UNKNOWN_PID
 pid_t to_pid(const std::string& value);
