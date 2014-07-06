45814fcb1b0a019c7eae05b5074a8afb8a617262
Holger Hoffstätte <holger.hoffstaette@googlemail.com>
2014-07-06 19:48:01 +0200

    replace use of C++ <regexp> with plain C: reduce bloat & faster

--- pid.cpp
+++ pid.cpp
@@ -1,16 +1,43 @@
 
-#include <regex>
 #include <string>
+#include <stdexcept>
+#include <regex.h>
 
 #include "pid.h"
 
 using namespace std;
 
-// for matching /proc/<pid> entries
-static const regex is_number("\\d+");
+static const char* PID_REGEXP = "^[0-9]+$";
+
+class Matcher
+{
+private:
+	regex_t _re;
+
+public:
+	Matcher()
+	{
+		if (regcomp(&_re, PID_REGEXP, REG_NOSUB|REG_EXTENDED) != 0)
+		{
+			throw runtime_error("internal error during regcomp()");
+		}
+	}
+
+	~Matcher()
+	{
+		regfree(&_re);
+	}
+
+	bool match(const string& value)
+	{
+		return (regexec(&_re, value.c_str(), 0, NULL, 0) == 0);
+	}
+};
+
+static Matcher _matcher;
 
 pid_t to_pid(const string& value)
 {
-	return regex_match(value, is_number) ? stoi(value) : UNKNOWN_PID;
+	return _matcher.match(value) ? stoi(value) : UNKNOWN_PID;
 }
 
