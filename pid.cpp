5525c19c56f60145cf35a4e29d465ca8c310eadf
Holger Hoffstätte <holger.hoffstaette@googlemail.com>
2014-08-01 17:27:25 +0200

    nuke the great regex experiment in favor of simplicity

--- pid.cpp
+++ pid.cpp
@@ -1,44 +1,20 @@
 
-#include <stdexcept>
+#include <algorithm>
+#include <cstring>
 #include <string>
-#include <regex.h>
 
 #include "pid.h"
 
 using namespace std;
 
-class Matcher
+static auto is_digit = [](char c)
 {
-private:
-	regex_t _re;
-
-public:
-	Matcher(const std::string& expression)
-	{
-		if (regcomp(&_re, expression.c_str(), REG_NOSUB|REG_EXTENDED) != 0)
-		{
-			throw runtime_error("internal error during regcomp()");
-		}
-	}
-
-	Matcher(const Matcher& other) = delete;
-	Matcher& operator = (const Matcher& other) = delete;
-
-	~Matcher()
-	{
-		regfree(&_re);
-	}
-
-	bool match(const std::string& value)
-	{
-		return (regexec(&_re, value.c_str(), 0, NULL, 0) == 0);
-	}
+	return isdigit(c);
 };
-
-static Matcher is_pid("^[0-9]+$");
 
 pid_t to_pid(const string& value)
 {
-	return is_pid.match(value) ? stoi(value) : UNKNOWN_PID;
+	bool valid = all_of(value.cbegin(), value.cend(), is_digit);
+	return valid ? stoi(value) : UNKNOWN_PID;
 }
 
